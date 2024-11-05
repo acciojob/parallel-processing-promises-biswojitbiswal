55d445fbbaffbef83adbc3957bfd98fb28ebe837
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-11-05 10:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+btn.addEventListener("click", () => {
+            downloadImages(images)
+                .then(imageElements => {
+                    imageElements.forEach(img => output.appendChild(img));
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        });
+
+        function downloadImages(imageArray) {
+            const promises = imageArray.map(image => {
+                return new Promise((resolve, reject) => {
+                    const img = new Image();
+                    img.src = image.url;
+                    img.onload = () => resolve(img);
+                    img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+                });
+            });
+
+            return Promise.all(promises);
+        }
